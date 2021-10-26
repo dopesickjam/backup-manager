631fdb4cc58dbc584b02a8a7f5d5fd593efdd243
Oleg Kandyba <o.kandyba@admitad.com>
2021-10-26 17:35:39 +0300

    add backups for psql

--- Readme.md
+++ Readme.md
@@ -2,3 +2,10 @@
 
 depencious:
 s3cmd need install on server
+for backup posgresql need user`s grants with no password local connections
+```
+> useradd backuper
+psql> create user backuper;
+psql> alter user backuper with superuser;
+> echo 'local   all             backuper                                peer' > /path/to/pg_hba.conf
+```
--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def foldertoS3(c, folder, server, s3_bucket, s3_path, s3cfg, backup_type, date):
     logging.info(f'Sync {folder} to S3 type is {backup_type}')
     c.sudo(f's3cmd -c {s3cfg} sync /tmp/{server}{folder} s3://{s3_bucket}/{s3_path}/{backup_type}/{date}{folder}')
 
-def rotateBackup(s3cfg, c, rotate_path, rotate_type, folder, backup_type):
+def rotateBackup(s3cfg, c, rotate_path, rotate_type, name, backup_type):
     s3_folder_list = c.sudo(f's3cmd -c {s3cfg} ls {rotate_path}/{rotate_type}/').stdout
 
     date_list = []
@@ -41,16 +41,20 @@ def rotateBackup(s3cfg, c, rotate_path, rotate_type, folder, backup_type):
             date_list.remove(oldest_folder)
 
             if backup_type == 'files':
-                c.sudo(f's3cmd -c {s3cfg} del --recursive {rotate_path}/{rotate_type}/{oldest_folder.strftime("%d_%m_%Y")}{folder}')
-            elif backup_type == 'mysql':
-                c.sudo(f's3cmd -c {s3cfg} del --recursive {rotate_path}/{rotate_type}/{oldest_folder.strftime("%d_%m_%Y")}/{folder}.sql.gz')
+                c.sudo(f's3cmd -c {s3cfg} del --recursive {rotate_path}/{rotate_type}/{oldest_folder.strftime("%d_%m_%Y")}{name}')
+            elif backup_type == 'mysql' or backup_type == 'postgres':
+                c.sudo(f's3cmd -c {s3cfg} del --recursive {rotate_path}/{rotate_type}/{oldest_folder.strftime("%d_%m_%Y")}/{name}.sql.gz')
         else:
             logging.info(f'All oldest backups was deleted')
             break
 
-def listDB(c, ignore_db, extrafile_path):
+def listDB(c, ignore_db, extrafile_path, backup_type):
     logging.info(f'Get list db')
-    db_list = c.sudo(f'mysql --defaults-extra-file={extrafile_path} -e "show databases;" | grep -v Database').stdout
+
+    if backup_type == 'mysql':
+        db_list = c.sudo(f'mysql --defaults-extra-file={extrafile_path} -e "show databases;" | grep -v Database').stdout
+    elif backup_type == 'postgres':
+        db_list = c.run(f'psql -d template1 -c "select datname from pg_database;" | grep -Ev "datname|row"| sed "1d"| cut -f2 -d" "').stdout
 
     db_list = db_list.strip().split('\n')
 
@@ -59,12 +63,17 @@ def listDB(c, ignore_db, extrafile_path):
 
     return db_list
 
-def backupDB(c, server, db, user):
+def backupDB(c, server, db, user, backup_type):
     logging.info(f'Backup: {db}')
     backup_dir = f'/tmp/{server}'
     c.sudo(f'mkdir -p {backup_dir}')
     c.sudo(f'chown {user}.{user} {backup_dir}')
-    c.sudo(f'mysqldump --defaults-extra-file={extrafile_path} --single-transaction {db} | gzip > {backup_dir}/{db}.sql.gz')
+
+    if backup_type == 'mysql':
+        c.sudo(f'mysqldump --defaults-extra-file={extrafile_path} --single-transaction {db} | gzip > {backup_dir}/{db}.sql.gz')
+    elif backup_type == 'postgres':
+        c.run(f'pg_dump {db} -F c -b -v > {backup_dir}/{db}.sql')
+        c.run(f'gzip -f {backup_dir}/{db}.sql')
 
 def dbtoS3(c, server, db, s3cfg, s3_bucket, s3_path, day_number, backup_type):
     logging.info(f'Put {db}.sql.gz to s3')
@@ -134,10 +143,11 @@ if args.backup_config:
                     if retain_monthly != 0:
                         rotate_type = 'monthly'
                         rotateBackup(s3cfg, c, rotate_path, rotate_type, folder, 'files')
+                    c.sudo(f'rm -rf {s3cfg}')
 
-                c.sudo(f'rm -rf {s3cfg}')
                 c.close()
                 logging.info(f'{server}: Rotation files was done')
+
             elif backup_type['type'] == 'mysql':
                 logging.info(f'{server}: Starting process for backup mysql')
                 c = Connect(server, port, user)
@@ -145,12 +155,12 @@ if args.backup_config:
                 extrafile_path = backup_type['extrafile_path']
                 ignore_db = backup_type['ignore_db']
 
-                list_db = listDB(c, ignore_db, extrafile_path)
+                list_db = listDB(c, ignore_db, extrafile_path, backup_type['type'])
 
                 for db in list_db:
                     c.put('.env', s3cfg)
 
-                    backupDB(c, server, db, user)
+                    backupDB(c, server, db, user, 'mysql')
 
                     backup_type = 'daily'
                     dbtoS3(c, server, db, s3cfg, s3_bucket, s3_path, day_number, backup_type)
@@ -187,4 +197,56 @@ if args.backup_config:
                         rotateBackup(s3cfg, c, rotate_path, rotate_type, db, 'mysql')
 
                     c.sudo(f'rm -rf {s3cfg}')
+                c.close()
+                logging.info(f'{server}: Rotation dbs was done')
+
+            elif backup_type['type'] == 'postgres':
+                logging.info(f'{server}: Starting process for backup postgres')
+                c = Connect(server, port, user)
+
+                ignore_db = backup_type['ignore_db']
+
+                list_db = listDB(c, ignore_db, None, backup_type['type'])
+
+                for db in list_db:
+                    c.put('.env', s3cfg)
+
+                    backupDB(c, server, db, user, backup_type['type'])
+
+                    backup_type = 'daily'
+                    dbtoS3(c, server, db, s3cfg, s3_bucket, s3_path, day_number, backup_type)
+
+                    if retain_weekly != 0:
+                        if datetime.datetime.today().weekday() == 5:
+                            backup_type = 'weekly'
+                            dbtoS3(c, server, db, s3cfg, s3_bucket, s3_path, day_number, backup_type)
+
+                    if retain_monthly != 0:
+                        if int(datetime.datetime.today().strftime("%d")) == 1:
+                            backup_type = 'monthly'
+                            dbtoS3(c, server, db, s3cfg, s3_bucket, s3_path, day_number, backup_type)
+
+                    c.sudo(f'rm -rf {s3cfg}')
+                    c.sudo(f'rm -rf /tmp/{server}')
+
+                logging.info(f'{server}: Backup dbs was done')
+
+                logging.info(f'{server}: Starting process for rotation dbs')
+                for db in list_db:
+                    c.put('.env', s3cfg)
+
+                    rotate_path = f's3://{s3_bucket}/{s3_path}'
+                    rotate_type = 'daily'
+                    rotateBackup(s3cfg, c, rotate_path, rotate_type, db, 'postgres')
+
+                    if retain_weekly != 0:
+                        rotate_type = 'weekly'
+                        rotateBackup(s3cfg, c, rotate_path, rotate_type, db, 'postgres')
+
+                    if retain_monthly != 0:
+                        rotate_type = 'monthly'
+                        rotateBackup(s3cfg, c, rotate_path, rotate_type, db, 'postgres')
+
+                    c.sudo(f'rm -rf {s3cfg}')
+                c.close()
                 logging.info(f'{server}: Rotation dbs was done')
